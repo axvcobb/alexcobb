9b35f55009393ad4fdd9d2a3d43079de0fa4f1aa
Alex Cobb <axvcobb@gmail.com>
2020-01-29 23:26:40 -0500

    Race create/update edits...Attempted datetime picker

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -21,7 +21,7 @@ class RacesController < ApplicationController
   end
 
   def create
-    @race = Race.new(post_params)
+    @race = Race.new(race_params)
 
     if @race.save
       redirect_to races_path
@@ -31,17 +31,29 @@ class RacesController < ApplicationController
   end
 
   def edit
+    @race = Race.find(params[:id])
   end
 
   def update
+    @race = Race.find(params[:id])
+
+    if @race.update(race_params)
+      redirect_to races_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @race = Race.find(params[:id])
+    @race.destroy
+
+    redirect_to races_path
   end
 
   private
-    def post_params
-      params.require(:race).permit(:name, :race_date, :city, :state,
+    def race_params
+      params.require(:race).permit(:name, :event, :race_date, :city, :state,
                                    :website, :comment)
     end
 end
