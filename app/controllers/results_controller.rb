1d31e58fa66d81093f40a10ea9e0406229245482
Alex Cobb <axvcobb@gmail.com>
2020-02-01 15:51:47 -0500

    Struggling to get results nesting to work :( More work to come later

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,18 @@
 class ResultsController < ApplicationController
+
+  def new
+    @race = Race.find(params[:race_id])
+    @result = @race.result.new
+  end
+
+  def create
+    @race = Race.find(params[:race_id])
+    @result = @race.result.create(result_params)
+    redirect_to races_path
+  end
+
+  private
+    def result_params
+      params.require(:result).permit(:gun_time, :chip_time)
+    end
 end
